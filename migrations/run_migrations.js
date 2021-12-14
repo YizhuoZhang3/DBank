a9b9ff50192aaa1d6ddea7c7eb79a98ccd181d4d
Zoe Zhang <zhangyizhuozoe@gmail.com>
2021-12-13 23:47:08 -0500

    added: Tether contract

--- migrations/run_migrations.js
+++ migrations/run_migrations.js
@@ -1,4 +1,6 @@
-const Migrations = artifacts.require('Migrations');
-module.exports = function deployer() {
-  deployer.deploy(Migrations)
-}
+const Migrations = artifacts.require("Migrations");
+
+module.exports = function(deployer) {
+  deployer.deploy(Migrations);
+};
+
