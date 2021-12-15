94e86788808ae959ec8e4fb9c77472d1d217b73f
Zoe Zhang <zhangyizhuozoe@gmail.com>
2021-12-15 00:25:43 -0500

    Updates

---
+++ precache-manifest.673446920372be3ebad8094ca4690635.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "94b26cf299e32958b043",
+    "url": "/DBank/static/js/main.94b26cf2.chunk.js"
+  },
+  {
+    "revision": "aa1499541f8a1cbe4808",
+    "url": "/DBank/static/css/1.5cdf1d27.chunk.css"
+  },
+  {
+    "revision": "aa1499541f8a1cbe4808",
+    "url": "/DBank/static/js/1.aa149954.chunk.js"
+  },
+  {
+    "revision": "09a7931bbd70ebbd41aa",
+    "url": "/DBank/static/js/runtime~main.09a7931b.js"
+  },
+  {
+    "revision": "87b262de9462d72de32804977834fd8e",
+    "url": "/DBank/static/media/bank.87b262de.png"
+  },
+  {
+    "revision": "70c709580509f0f8b521888824bef57d",
+    "url": "/DBank/static/media/tether.70c70958.png"
+  },
+  {
+    "revision": "6aaf4f0a17fea0adf17ee10d4585a71a",
+    "url": "/DBank/index.html"
+  }
+];
